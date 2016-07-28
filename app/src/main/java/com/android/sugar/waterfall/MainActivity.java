15cfbe654f16a2d450395d86d815faae4b2b4c73
Sugar.T <SugarT@SugarTdeMacBook-Pro.local>
2016-07-28 10:02:28 +0800

    add load local images

--- app/src/main/java/com/android/sugar/waterfall/MainActivity.java
+++ app/src/main/java/com/android/sugar/waterfall/MainActivity.java
@@ -1,6 +1,7 @@
 package com.android.sugar.waterfall;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.RecyclerView;
@@ -36,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
 
         RecyclerView.LayoutManager layoutManager = new StaggeredGridLayoutManager(mColumsCount,
                 StaggeredGridLayoutManager.VERTICAL);
+        ((StaggeredGridLayoutManager)layoutManager).setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_NONE);
+//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, true);
         mContentRv.setLayoutManager(layoutManager);
         mAdapter = new WaterfallAdapter(this, mDataSources);
         mContentRv.setAdapter(mAdapter);
@@ -47,36 +50,167 @@ public class MainActivity extends AppCompatActivity {
         mContentRv = (RecyclerView) findViewById(R.id.rv_main_content);
     }
 
+//    private void initSource() {
+//        mDataSources.add("http://d.hiphotos.baidu.com/zhidao/pic/item/a1ec08fa513d2697fa44b40953fbb2fb4216d8cb.jpg");
+//        mDataSources.add("http://d.hiphotos.baidu" +
+//                ".com/zhidao/wh%3D450%2C600/sign=d8c645729513b07ebde8580c39e7bd15" +
+//                "/a8014c086e061d95294ff56b7df40ad162d9ca5c.jpg");
+//        mDataSources.add("http://a.hiphotos.baidu" +
+//                ".com/zhidao/wh%3D450%2C600/sign=3757236433fae6cd0ce1a3653a832312" +
+//                "/bd315c6034a85edf2dcd3c704f540923dd547524.jpg");
+//        mDataSources.add("http://g.hiphotos.baidu.com/zhidao/pic/item/50da81cb39dbb6fdc4b794a70824ab18962b3746.jpg");
+//        mDataSources.add("http://g.hiphotos.baidu.com/zhidao/pic/item/37d12f2eb9389b50a9794db98335e5dde6116e53.jpg");
+//        mDataSources.add("http://c.hiphotos.baidu.com/zhidao/pic/item/91529822720e0cf395f783a80c46f21fbe09aa65.jpg");
+//        mDataSources.add("http://c.hiphotos.baidu.com/zhidao/pic/item/242dd42a2834349b118764e3caea15ce37d3bed6.jpg");
+//        mDataSources.add("http://pic67.nipic.com/file/20150514/21036787_181947848862_2.jpg");
+//        mDataSources.add("http://b.hiphotos.baidu.com/zhidao/pic/item/7dd98d1001e9390190e87f187fec54e736d1962a.jpg");
+//        mDataSources.add("http://img.qqtz.com/K2014/0614/20140614123823930.jpg");
+//        mDataSources.add("http://a.hiphotos.baidu.com/zhidao/pic/item/d52a2834349b033b5b349bfe16ce36d3d539bd51.jpg");
+//        mDataSources.add("http://g.hiphotos.baidu" +
+//                ".com/zhidao/wh%3D450%2C600/sign=bfeab173c5cec3fd8b6baf71e3b8f809" +
+//                "/2e2eb9389b504fc2d39afcbae3dde71191ef6d43.jpg");
+//        mDataSources.add("http://b.hiphotos.baidu" +
+//                ".com/zhidao/wh%3D450%2C600/sign=6046b2f46509c93d07a706f3aa0dd4ea" +
+//                "/4a36acaf2edda3ccd58840da03e93901203f92d8.jpg");
+//        mDataSources.add("http://a.hiphotos.baidu.com/zhidao/pic/item/4ec2d5628535e5dd773a631e73c6a7efcf1b62d9.jpg");
+//        mDataSources.add("http://img.pconline.com.cn/images/upload/upc/tx/wallpaper/1302/04/c0/17981969_1359970249171" +
+//                ".jpg");
+//        mDataSources.add("http://imga.mumayi.com/android/wallpaper/2012/01/21/sl_600_2012012105503827801816.jpg");
+//        mDataSources.add("http://d.hiphotos.baidu.com/zhidao/pic/item/a2cc7cd98d1001e972ed0b3fbe0e7bec54e79728.jpg");
+//        mDataSources.add("http://f.hiphotos.baidu.com/zhidao/pic/item/9358d109b3de9c8264e023df6f81800a18d843dd.jpg");
+//        mDataSources.add("http://mg.soupingguo.com/bizhi/big/10/133/531/10133531.jpg");
+//        mDataSources.add("http://imgsrc.baidu" +
+//                ".com/forum/w=580/sign=34f7ea61d443ad4ba62e46c8b2005a89/78e5fc039245d68839fba560a1c27d1ed31b2415.jpg");
+//        mDataSources.add("http://d.hiphotos.baidu.com/zhidao/pic/item/a1ec08fa513d2697fa44b40953fbb2fb4216d8cb.jpg");
+//        mDataSources.add("http://d.hiphotos.baidu" +
+//                ".com/zhidao/wh%3D450%2C600/sign=d8c645729513b07ebde8580c39e7bd15" +
+//                "/a8014c086e061d95294ff56b7df40ad162d9ca5c.jpg");
+//        mDataSources.add("http://a.hiphotos.baidu" +
+//                ".com/zhidao/wh%3D450%2C600/sign=3757236433fae6cd0ce1a3653a832312" +
+//                "/bd315c6034a85edf2dcd3c704f540923dd547524.jpg");
+//        mDataSources.add("http://g.hiphotos.baidu.com/zhidao/pic/item/50da81cb39dbb6fdc4b794a70824ab18962b3746.jpg");
+//        mDataSources.add("http://g.hiphotos.baidu.com/zhidao/pic/item/37d12f2eb9389b50a9794db98335e5dde6116e53.jpg");
+//        mDataSources.add("http://c.hiphotos.baidu.com/zhidao/pic/item/91529822720e0cf395f783a80c46f21fbe09aa65.jpg");
+//        mDataSources.add("http://c.hiphotos.baidu.com/zhidao/pic/item/242dd42a2834349b118764e3caea15ce37d3bed6.jpg");
+//        mDataSources.add("http://pic67.nipic.com/file/20150514/21036787_181947848862_2.jpg");
+//        mDataSources.add("http://b.hiphotos.baidu.com/zhidao/pic/item/7dd98d1001e9390190e87f187fec54e736d1962a.jpg");
+//        mDataSources.add("http://img.qqtz.com/K2014/0614/20140614123823930.jpg");
+//        mDataSources.add("http://a.hiphotos.baidu.com/zhidao/pic/item/d52a2834349b033b5b349bfe16ce36d3d539bd51.jpg");
+//        mDataSources.add("http://g.hiphotos.baidu" +
+//                ".com/zhidao/wh%3D450%2C600/sign=bfeab173c5cec3fd8b6baf71e3b8f809" +
+//                "/2e2eb9389b504fc2d39afcbae3dde71191ef6d43.jpg");
+//        mDataSources.add("http://b.hiphotos.baidu" +
+//                ".com/zhidao/wh%3D450%2C600/sign=6046b2f46509c93d07a706f3aa0dd4ea" +
+//                "/4a36acaf2edda3ccd58840da03e93901203f92d8.jpg");
+//        mDataSources.add("http://a.hiphotos.baidu.com/zhidao/pic/item/4ec2d5628535e5dd773a631e73c6a7efcf1b62d9.jpg");
+//        mDataSources.add("http://img.pconline.com.cn/images/upload/upc/tx/wallpaper/1302/04/c0/17981969_1359970249171" +
+//                ".jpg");
+//        mDataSources.add("http://imga.mumayi.com/android/wallpaper/2012/01/21/sl_600_2012012105503827801816.jpg");
+//        mDataSources.add("http://d.hiphotos.baidu.com/zhidao/pic/item/a2cc7cd98d1001e972ed0b3fbe0e7bec54e79728.jpg");
+//        mDataSources.add("http://f.hiphotos.baidu.com/zhidao/pic/item/9358d109b3de9c8264e023df6f81800a18d843dd.jpg");
+//        mDataSources.add("http://mg.soupingguo.com/bizhi/big/10/133/531/10133531.jpg");
+//        mDataSources.add("http://imgsrc.baidu" +
+//                ".com/forum/w=580/sign=34f7ea61d443ad4ba62e46c8b2005a89/78e5fc039245d68839fba560a1c27d1ed31b2415.jpg");
+//        mDataSources.add("http://d.hiphotos.baidu.com/zhidao/pic/item/a1ec08fa513d2697fa44b40953fbb2fb4216d8cb.jpg");
+//        mDataSources.add("http://d.hiphotos.baidu" +
+//                ".com/zhidao/wh%3D450%2C600/sign=d8c645729513b07ebde8580c39e7bd15" +
+//                "/a8014c086e061d95294ff56b7df40ad162d9ca5c.jpg");
+//        mDataSources.add("http://a.hiphotos.baidu" +
+//                ".com/zhidao/wh%3D450%2C600/sign=3757236433fae6cd0ce1a3653a832312" +
+//                "/bd315c6034a85edf2dcd3c704f540923dd547524.jpg");
+//        mDataSources.add("http://g.hiphotos.baidu.com/zhidao/pic/item/50da81cb39dbb6fdc4b794a70824ab18962b3746.jpg");
+//        mDataSources.add("http://g.hiphotos.baidu.com/zhidao/pic/item/37d12f2eb9389b50a9794db98335e5dde6116e53.jpg");
+//        mDataSources.add("http://c.hiphotos.baidu.com/zhidao/pic/item/91529822720e0cf395f783a80c46f21fbe09aa65.jpg");
+//        mDataSources.add("http://c.hiphotos.baidu.com/zhidao/pic/item/242dd42a2834349b118764e3caea15ce37d3bed6.jpg");
+//        mDataSources.add("http://pic67.nipic.com/file/20150514/21036787_181947848862_2.jpg");
+//        mDataSources.add("http://b.hiphotos.baidu.com/zhidao/pic/item/7dd98d1001e9390190e87f187fec54e736d1962a.jpg");
+//        mDataSources.add("http://img.qqtz.com/K2014/0614/20140614123823930.jpg");
+//        mDataSources.add("http://a.hiphotos.baidu.com/zhidao/pic/item/d52a2834349b033b5b349bfe16ce36d3d539bd51.jpg");
+//        mDataSources.add("http://g.hiphotos.baidu" +
+//                ".com/zhidao/wh%3D450%2C600/sign=bfeab173c5cec3fd8b6baf71e3b8f809" +
+//                "/2e2eb9389b504fc2d39afcbae3dde71191ef6d43.jpg");
+//        mDataSources.add("http://b.hiphotos.baidu" +
+//                ".com/zhidao/wh%3D450%2C600/sign=6046b2f46509c93d07a706f3aa0dd4ea" +
+//                "/4a36acaf2edda3ccd58840da03e93901203f92d8.jpg");
+//        mDataSources.add("http://a.hiphotos.baidu.com/zhidao/pic/item/4ec2d5628535e5dd773a631e73c6a7efcf1b62d9.jpg");
+//        mDataSources.add("http://img.pconline.com.cn/images/upload/upc/tx/wallpaper/1302/04/c0/17981969_1359970249171" +
+//                ".jpg");
+//        mDataSources.add("http://imga.mumayi.com/android/wallpaper/2012/01/21/sl_600_2012012105503827801816.jpg");
+//        mDataSources.add("http://d.hiphotos.baidu.com/zhidao/pic/item/a2cc7cd98d1001e972ed0b3fbe0e7bec54e79728.jpg");
+//        mDataSources.add("http://f.hiphotos.baidu.com/zhidao/pic/item/9358d109b3de9c8264e023df6f81800a18d843dd.jpg");
+//        mDataSources.add("http://mg.soupingguo.com/bizhi/big/10/133/531/10133531.jpg");
+//        mDataSources.add("http://imgsrc.baidu" +
+//                ".com/forum/w=580/sign=34f7ea61d443ad4ba62e46c8b2005a89/78e5fc039245d68839fba560a1c27d1ed31b2415.jpg");
+//    }
+
     private void initSource() {
-        mDataSources.add("http://d.hiphotos.baidu.com/zhidao/pic/item/a1ec08fa513d2697fa44b40953fbb2fb4216d8cb.jpg");
-        mDataSources.add("http://d.hiphotos.baidu" +
-                ".com/zhidao/wh%3D450%2C600/sign=d8c645729513b07ebde8580c39e7bd15" +
-                "/a8014c086e061d95294ff56b7df40ad162d9ca5c.jpg");
-        mDataSources.add("http://a.hiphotos.baidu" +
-                ".com/zhidao/wh%3D450%2C600/sign=3757236433fae6cd0ce1a3653a832312" +
-                "/bd315c6034a85edf2dcd3c704f540923dd547524.jpg");
-        mDataSources.add("http://g.hiphotos.baidu.com/zhidao/pic/item/50da81cb39dbb6fdc4b794a70824ab18962b3746.jpg");
-        mDataSources.add("http://g.hiphotos.baidu.com/zhidao/pic/item/37d12f2eb9389b50a9794db98335e5dde6116e53.jpg");
-        mDataSources.add("http://c.hiphotos.baidu.com/zhidao/pic/item/91529822720e0cf395f783a80c46f21fbe09aa65.jpg");
-        mDataSources.add("http://c.hiphotos.baidu.com/zhidao/pic/item/242dd42a2834349b118764e3caea15ce37d3bed6.jpg");
-        mDataSources.add("http://pic67.nipic.com/file/20150514/21036787_181947848862_2.jpg");
-        mDataSources.add("http://b.hiphotos.baidu.com/zhidao/pic/item/7dd98d1001e9390190e87f187fec54e736d1962a.jpg");
-        mDataSources.add("http://img.qqtz.com/K2014/0614/20140614123823930.jpg");
-        mDataSources.add("http://a.hiphotos.baidu.com/zhidao/pic/item/d52a2834349b033b5b349bfe16ce36d3d539bd51.jpg");
-        mDataSources.add("http://g.hiphotos.baidu" +
-                ".com/zhidao/wh%3D450%2C600/sign=bfeab173c5cec3fd8b6baf71e3b8f809" +
-                "/2e2eb9389b504fc2d39afcbae3dde71191ef6d43.jpg");
-        mDataSources.add("http://b.hiphotos.baidu" +
-                ".com/zhidao/wh%3D450%2C600/sign=6046b2f46509c93d07a706f3aa0dd4ea" +
-                "/4a36acaf2edda3ccd58840da03e93901203f92d8.jpg");
-        mDataSources.add("http://a.hiphotos.baidu.com/zhidao/pic/item/4ec2d5628535e5dd773a631e73c6a7efcf1b62d9.jpg");
-        mDataSources.add("http://img.pconline.com.cn/images/upload/upc/tx/wallpaper/1302/04/c0/17981969_1359970249171" +
-                ".jpg");
-        mDataSources.add("http://imga.mumayi.com/android/wallpaper/2012/01/21/sl_600_2012012105503827801816.jpg");
-        mDataSources.add("http://d.hiphotos.baidu.com/zhidao/pic/item/a2cc7cd98d1001e972ed0b3fbe0e7bec54e79728.jpg");
-        mDataSources.add("http://f.hiphotos.baidu.com/zhidao/pic/item/9358d109b3de9c8264e023df6f81800a18d843dd.jpg");
-        mDataSources.add("http://mg.soupingguo.com/bizhi/big/10/133/531/10133531.jpg");
-        mDataSources.add("http://imgsrc.baidu" +
-                ".com/forum/w=580/sign=34f7ea61d443ad4ba62e46c8b2005a89/78e5fc039245d68839fba560a1c27d1ed31b2415.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/1.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/2.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/3.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/4.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/5.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/6.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/7.jpeg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/8.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/9.jpeg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/10.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/11.jpeg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/12.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/13.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/1.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/2.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/3.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/4.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/5.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/6.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/7.jpeg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/8.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/9.jpeg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/10.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/11.jpeg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/12.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/13.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/1.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/2.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/3.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/4.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/5.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/6.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/7.jpeg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/8.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/9.jpeg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/10.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/11.jpeg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/12.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/13.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/1.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/2.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/3.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/4.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/5.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/6.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/7.jpeg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/8.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/9.jpeg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/10.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/11.jpeg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/12.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/13.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/1.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/2.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/3.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/4.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/5.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/6.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/7.jpeg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/8.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/9.jpeg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/10.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/11.jpeg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/12.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/13.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/1.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/2.jpg");
+        mDataSources.add(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pics/3.jpg");
     }
 }
--- app/src/main/java/com/android/sugar/waterfall/WaterfallAdapter.java
+++ app/src/main/java/com/android/sugar/waterfall/WaterfallAdapter.java
@@ -1,6 +1,7 @@
 package com.android.sugar.waterfall;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
@@ -10,9 +11,6 @@ import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
-import com.bumptech.glide.request.animation.GlideAnimation;
-import com.bumptech.glide.request.target.SimpleTarget;
 
 import java.util.List;
 
@@ -54,22 +52,40 @@ public class WaterfallAdapter extends RecyclerView.Adapter<WaterfallAdapter.Wate
     @Override
     public void onBindViewHolder(final WaterfallVH holder, int position) {
 //        Glide.with(mContext).load(mImageArr.get(position)).into(holder.mContentIv);
-        Glide.with(mContext).load(mImageArr.get(position)).into(new SimpleTarget<GlideDrawable>() {
-            @Override
-            public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
-                ViewGroup.LayoutParams lp = holder.mContentIv.getLayoutParams();
 
-                WindowManager wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
-                DisplayMetrics outMetrics = new DisplayMetrics();
-                wm.getDefaultDisplay().getMetrics(outMetrics);
-                lp.width = outMetrics.widthPixels / 3;
-                lp.height = resource.getIntrinsicHeight() * outMetrics.widthPixels / 3 / resource.getIntrinsicWidth();
+        /* 网络图片 */
+//        Glide.with(mContext).load(mImageArr.get(position)).into(new SimpleTarget<GlideDrawable>() {
+//            @Override
+//            public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+//                ViewGroup.LayoutParams lp = holder.mContentIv.getLayoutParams();
+//
+//                WindowManager wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+//                DisplayMetrics outMetrics = new DisplayMetrics();
+//                wm.getDefaultDisplay().getMetrics(outMetrics);
+//                lp.width = outMetrics.widthPixels / 3;
+//                lp.height = resource.getIntrinsicHeight() * outMetrics.widthPixels / 3 / resource.getIntrinsicWidth();
+//
+//                holder.mContentIv.setLayoutParams(lp);
+//
+//                holder.mContentIv.setImageDrawable(resource);
+//            }
+//        });
 
-                holder.mContentIv.setLayoutParams(lp);
+        /* 本地图片 */
+        WindowManager wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
 
-                holder.mContentIv.setImageDrawable(resource);
-            }
-        });
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        opts.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mImageArr.get(position), opts);
+
+        ViewGroup.LayoutParams lp = holder.mContentIv.getLayoutParams();
+        lp.width = outMetrics.widthPixels / 3;
+        lp.height = opts.outHeight * outMetrics.widthPixels / 3 / opts.outWidth;
+        holder.mContentIv.setLayoutParams(lp);
+
+        Glide.with(mContext).load(mImageArr.get(position)).into(holder.mContentIv);
 
         /* 添加item点击事件 */
         holder.mContentIv.setOnClickListener(new View.OnClickListener() {
